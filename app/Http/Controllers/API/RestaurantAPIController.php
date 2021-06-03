dd72aee3f19b933651cb0d24540a92eeeed14bd0
devdiginest <shyju@onetikk.com>
2021-06-03 22:50:34 +0530

    error fixed in restaurant api controller

--- app/Http/Controllers/API/RestaurantAPIController.php
+++ app/Http/Controllers/API/RestaurantAPIController.php
@@ -119,7 +119,7 @@ class RestaurantAPIController extends Controller
         //         ->with('foods')->get();
         $foods = Food::join('restaurant_foods','foods.id','=','restaurant_foods.food_id')
         ->where('restaurant_foods.restaurant_id','=',$rid)->get();
-        foreach ($foods as $food {
+        foreach ($foods as $food ){
             // code...
             $food->food_id = $food->id;
         }
